ac8ad3183780bca325fbb14bee08b8dc337e6697
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2022-04-27 11:44:32 +0300

    refactor

--- src/test/java/yandex/oshkin/tests/commonsteps/CommonStepsAPI.java
+++ src/test/java/yandex/oshkin/tests/commonsteps/CommonStepsAPI.java
@@ -29,6 +29,7 @@ public class CommonStepsAPI extends TestBase {
                     Map<String, String> sessionCookies =
                             given()
                                     .log().all()
+                                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36" )
                                     .when()
                                     .get("")
                                     .then()
